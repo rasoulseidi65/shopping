a695ad1ffaa9ac9395cff8c45d0534d59679af06
mzeinoddini74 <m.zeinoddini74@gmail.com>
2020-11-10 21:01:29 +0330

    seller

--- src/app/Core/dashboard-seller/product-seller/product-seller.component.ts
+++ src/app/Core/dashboard-seller/product-seller/product-seller.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {SellerService} from '../seller.service';
-import {MessageService} from 'primeng/api';
+import {ConfirmationService, MessageService} from 'primeng/api';
 import {Router} from '@angular/router';
 import {SellerModel} from '../SellerModel';
 
@@ -9,7 +9,8 @@ import {SellerModel} from '../SellerModel';
   templateUrl: './product-seller.component.html',
   styleUrls: ['./product-seller.component.css'],
   providers: [
-    MessageService
+    MessageService,
+    ConfirmationService
   ]
 })
 export class ProductSellerComponent implements OnInit {
@@ -20,7 +21,8 @@ export class ProductSellerComponent implements OnInit {
   loading = false;
   constructor(private sellerService: SellerService,
               private messageService: MessageService,
-              private router: Router) {
+              private router: Router,
+              private confirmationService: ConfirmationService) {
   }
 
   ngOnInit(): void {
@@ -45,6 +47,24 @@ export class ProductSellerComponent implements OnInit {
         this.products = response.data;
       } else {
         this.messageService.add({severity: 'error', summary: ' دریافت اطلاعات ', detail: response.data});
+      }
+    });
+  }
+
+  delete(id: any): void {
+    this.confirmationService.confirm({
+      message: 'آیا از حذف رکورد انتخابی مطمین هستید؟',
+      header: 'تایید حذف',
+      icon: 'pi pi-exclamation-triangle',
+      rejectLabel: 'خیر',
+      acceptLabel: 'بله',
+      defaultFocus: 'reject',
+      accept: () => {
+        // delete from db
+      },
+      reject: () => {
+        // close
+        this.confirmationService.close();
       }
     });
   }
